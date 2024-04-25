34a6c15827bf9622d5459c5569600bcb65108f68
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2024-04-25 11:51:57 +0200

    Add project files.

---
+++ weather2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace weather2
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void hozzaad_click(object sender, RoutedEventArgs e)
+		{
+            if (txtBox.Text != "")
+            {
+				list.Items.Add(txtBox.Text);
+            }
+			else
+			{
+				MessageBox.Show("írjál valamit be");
+			}
+        }
+	}
+}
