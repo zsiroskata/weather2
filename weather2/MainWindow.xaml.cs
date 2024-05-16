457ca46dfabab27986ad7d131343ca1ba0307b38
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2024-05-16 14:03:34 +0200

    valami

--- weather2/MainWindow.xaml.cs
+++ weather2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,20 +17,39 @@ namespace weather2
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		List<Weather> varosok = new();
 		public MainWindow()
 		{
 			InitializeComponent();
-		}
+
+			using StreamReader sr = new StreamReader(
+				path: @"..\..\..\src\idojarasok.txt",
+				encoding: System.Text.Encoding.UTF8);
+			while ( !sr.EndOfStream ) 
+			{ 
+				varosok.Add(new Weather( sr.ReadLine()) );
+			}
+			sr.Close();
+
+            foreach (var item in varosok)
+            {
+                list.Items.Add( item.Varos );
+            }
+
+        }
 
 		private void hozzaad_click(object sender, RoutedEventArgs e)
 		{
-            if (txtBox.Text != "")
+            if (txtBox.Text != "" && txtBoxH.Text != "" && txtBoxP.Text != "")
             {
 				list.Items.Add(txtBox.Text);
+				varosok.Add(new Weather(txtBox.Text));
+				varosok.Add(new Weather(txtBoxH.Text ));
+				varosok.Add(new Weather(txtBoxP.Text ));
             }
 			else
 			{
-				MessageBox.Show("írjál valamit be");
+				MessageBox.Show("valamelyik adat hiányzik");
 			}
         }
 
@@ -40,5 +60,17 @@ namespace weather2
 				list.Items.Remove(list.SelectedItem);
 			}
 		}
-	}
+
+        private void list_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (list.SelectedItem != null)
+			{
+				int index = list.SelectedIndex;
+                homersekletLabel.Content = varosok[index].Homerseklet + "°C";
+				paratartalomLabel.Content = varosok[index].Paratartalom + "%";
+               
+
+            }
+        }
+    }
 }
--- weather2/Weather.cs
+++ weather2/Weather.cs
@@ -9,5 +9,16 @@ namespace weather2
 	internal class Weather
 	{
 		public string Varos { get; set; }
-	}
+		public double Homerseklet { get; set; }
+		public int Paratartalom { get; set; }
+		
+
+		public Weather(string sorok) 
+		{
+			var sor = sorok.Split(';');
+			Varos = sor[0];
+			Homerseklet	= Convert.ToDouble(sor[1]);
+			Paratartalom = Convert.ToInt32(sor[2]);
+		}
+    }
 }
